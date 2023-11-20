9ead288c4611f986864471a5c6365e4ad6b144f9
justin0x7 <imasnpsorrels5@gmail.com>
2023-11-20 17:23:27 +0900

    live server

--- src/core/constants/base.const.ts
+++ src/core/constants/base.const.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.REACT_APP_API_URL || "https://test.survey.eagle.production";
+export const API_URL = process.env.REACT_APP_API_URL || "https://test.survey.eagle.productions";
 export const QUESTIONNAIRES_URL = process.env.REACT_APP_QUESTIONNAIRES_URL || "https://test.bof.vallentuna.eagle.productions";
 export const QUESTIONNAIRES_URL1 = process.env.REACT_APP_QUESTIONNAIRES_URL1 || "https://test.vux.vallentuna.eagle.productions";
 // export const QUESTIONNAIRES_URL1 = process.env.REACT_APP_QUESTIONNAIRES_URL1 || "http://localhost:3000";
